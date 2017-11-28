f6b2fa4c019bf8de11bead598175b1e22c1c2c8e
ssbx <sebastien.serre@sysmo.io>
2017-11-28 13:57:06 +0100

    change tests output

--- tests/test_suite.sh
+++ tests/test_suite.sh
@@ -2,12 +2,15 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+echo "=> testing begin"
 T1="$(${DIR}/testChealpixNeighboursNest | md5sum | awk '{ print $1}')"
 T2="$(md5sum ${DIR}/data/tests_neighbours_nside16_nested.out | awk '{ print $1}')"
 if [ "${T1}" != "${T2}" ]
 then 
-	echo "Test for testChealpixNeighboursNest has failed"
+	echo "=> Test for testChealpixNeighboursNest has failed"
 	false;
 else
+	echo "=> Succesfull test testChealpixNeighboursNest"
 	true
 fi
+echo "=> testing end"
