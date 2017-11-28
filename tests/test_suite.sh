7e16753844c184d94e3d4ebb68805e0753a20104
ssbx <sebastien.serre@sysmo.io>
2017-11-28 14:08:16 +0100

    exit failure on test failure

--- tests/test_suite.sh
+++ tests/test_suite.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+STATUS=0
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "=> testing begin"
@@ -8,9 +10,9 @@ T2="$(md5sum ${DIR}/data/tests_neighbours_nside16_nested.out | awk '{ print $1}'
 if [ "${T1}" != "${T2}" ]
 then 
 	echo "=> Test for testChealpixNeighboursNest has failed"
-	false;
+	STATUS=1
 else
 	echo "=> Succesfull test testChealpixNeighboursNest"
-	true
 fi
 echo "=> testing end"
+exit $STATUS
